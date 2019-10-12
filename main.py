29570ddc73133f015d1bb555bfdc2124fc8778cd
Your Name <you@example.com>
2019-10-12 07:53:04 +0200

    add init func

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 class Book:
-    title = "no title"
-    author = "no author"
-    sbn = 0
+    def __init__(self, title, author, sbn):
+    self.title = title
+    self.author = author
+    self.sbn = sbn
 
-harry_potter = Book
+
+harry_potter = Book()
+print(harry_potter.title)
+print(harry_potter.author)
+print(harry_potter.sbn)
